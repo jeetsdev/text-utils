0ed08572a6cf851bf03735c043f772340dd6ce32
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2021-12-26 09:44:12 +0530

    Feat : clear text

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,6 +14,10 @@ export default function Main() {
         setText(newText);
     }
 
+    function clearTextHandler() {
+        setText("");
+    }
+
     function inputHandler(event) {
         setText(event.target.value);
     }
@@ -21,9 +25,10 @@ export default function Main() {
         <>
             <div className="mb-3">
                 <h1 className='text-center my-3'>Enter Your Text Here</h1>
-                <textarea className="form-control shadow-none" id="exampleFormControlTextarea1" rows="8" onChange={inputHandler} value={text} unselectable='h'></textarea>
+                <textarea className="form-control shadow-none" id="exampleFormControlTextarea1" rows="6" onChange={inputHandler} value={text} unselectable='h' style={{resize:'none'}}></textarea>
                 <button className='btn btn-primary my-3 mx-2' onClick={uppperClickHandler}>Uppercase</button>
                 <button className='btn btn-primary my-3 mx-2' onClick={lowerClickHandler}>Lowercase</button>
+                <button className='btn btn-primary my-3 mx-2' onClick={clearTextHandler}>Clear Text</button>
             </div>
             <div className="container">
                 <h2>Text Summary</h2>
@@ -33,6 +38,10 @@ export default function Main() {
                 <p>
                     Total {Math.floor(text.split(" ").length/150)} Minutes read.
                 </p>
+            </div>
+            <div className="container">
+                <h3>Preview</h3>
+                <p>{ text}</p>
             </div>
         </>
     )
